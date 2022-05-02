64240759d7bf99a0a13552e4cd74fe77d7ffb59f
Wong Keng <kaira428@gmail.com>
2022-05-02 12:03:50 +0800

    Initial commit

---
+++ src/components/Cart/CartButton.js
@@ -0,0 +1,23 @@
+import { useDispatch, useSelector } from "react-redux";
+import { uiActions } from "../../store/ui-slice";
+
+import classes from "./CartButton.module.css";
+
+const CartButton = (props) => {
+  const dispatch = useDispatch();
+
+  const cartQuantity = useSelector((state) => state.cart.totalQuantity);
+
+  const buttonClickHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+
+  return (
+    <button className={classes.button} onClick={buttonClickHandler}>
+      <span>My Cart</span>
+      <span className={classes.badge}>{cartQuantity}</span>
+    </button>
+  );
+};
+
+export default CartButton;
